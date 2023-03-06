1769a8bd80ab9d94c5bff2b11d95612afb0628f2
Gabriel Martins <gabrield_martins@hotmail.com>
2023-03-06 20:49:52 -0300

    transformando o avatar em um componente

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from '../Avatar'
 import styles from './Comment.module.css'
 
 export function Comment() {
     return(
         <div className={styles.comment} >
-            <img src="https://github.com/Gab360riel.png"/>
+            <Avatar hasBorder={false} src="https://github.com/Gab360riel.png"/>
 
             <div className={styles.commentBox} >
                 <div className={styles.commentContent}>
